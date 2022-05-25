6fd36505f5ee187e768ef18ceb990d167859d17f
Gaurav kanojia <kanojia.gaurav111@gmail.com>
2022-05-25 12:45:01 +0530

    practice

---
+++ two-sum.cpp
@@ -0,0 +1,10 @@
+vector <int> v;
+      for(int i=0;i<nums.size();i++){
+          for(int j=i+1;j<nums.size();j++){
+              if(nums[i]+nums[j]==target){
+                  v.push_back(i);
+                  v.push_back(j);
+              }
+          }
+      }
+       return v;
